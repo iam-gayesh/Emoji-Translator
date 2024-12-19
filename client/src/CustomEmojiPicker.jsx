7ae897d8ac677093dd2338fc643912621d48069a
gayesh_p <gayesh_p@epiclanka.net>
2024-12-19 12:21:05 +0530

    final changes from forntend

--- client/src/CustomEmojiPicker.jsx
+++ client/src/CustomEmojiPicker.jsx
@@ -1,11 +1,22 @@
 import EmojiPicker from "emoji-picker-react";
 import axios from "axios";
+import { useState } from "react";
 
 const CustomEmojiPicker = () => {
+  const [selectedEmoji, setSelectedEmoji] = useState("");
+
   const onEmojiClick = (event, emojiObject) => {
-    axios.post("http://localhost:3001/translate", {
-      emojiSequence: emojiObject.emoji,
-    });
+    setSelectedEmoji(emojiObject.emoji);
+    axios
+      .post("http://localhost:3001/translate", {
+        emojiSequence: emojiObject.emoji,
+      })
+      .then((response) => {
+        console.log(response.data.translation);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
@@ -14,14 +25,32 @@ const CustomEmojiPicker = () => {
         <h1 className="justify-content-center">Emoji-Translator</h1>
         <div className="d-flex flex-direction-column mt-3">
           <div className="justify-content-center mt-3 d-flex flex-direction-row">
-            <EmojiPicker />
+            <EmojiPicker onEmojiClick={onEmojiClick} />
           </div>
 
           <div>
             <div className="d-flex flex-direction-column">
+              {selectedEmoji && (
+                <div className="d-flex flex-direction-column">
+                  <h3>Selected Emoji</h3>
+                  <div className="bg-white p-3 rounded">
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={selectedEmoji}
+                      readOnly
+                    />
+                  </div>
+                </div>
+              )}
               <h3>Selected Emoji</h3>
               <div className="bg-white p-3 rounded">
-                <input type="text" className="form-control" />
+                <input
+                  type="text"
+                  className="form-control"
+                  value={selectedEmoji}
+                  readOnly
+                />
               </div>
             </div>
 
